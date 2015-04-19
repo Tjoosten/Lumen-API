25b9f2ade37490c1f908ad0000c6b3fdb7db8933
Tjoosten <Tjoosten3@gmail.com>
2015-04-19 18:12:53 +0200

    Add regimenten migration

---
+++ API - source code/database/migrations/2015_04_19_114427_Regimenten.php
@@ -0,0 +1,32 @@
+<?php
+
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Database\Migrations\Migration;
+
+	class Regimenten extends Migration {
+
+		/**
+		 * Run the migrations.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		public function up() {
+			Schema::create('Regimenten', function($table) {
+				$table->increments('id');
+				$table->string('Regiment');
+				$table->timestamps();
+			});
+		}
+
+		/**
+		 * Reverse the migrations.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		public function down() {
+			Schema::drop('Regimenten');
+		}
+
+	}
