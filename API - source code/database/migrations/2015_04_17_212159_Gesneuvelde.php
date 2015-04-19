c36299c64242959b1c05dc8e1e664d009cf26193
Tjoosten <Tjoosten3@gmail.com>
2015-04-19 13:43:14 +0200

    Add gesneuvelde migration

--- API - source code/database/migrations/2015_04_17_212159_Gesneuvelde.php
+++ API - source code/database/migrations/2015_04_17_212159_Gesneuvelde.php
@@ -8,19 +8,44 @@
 		/**
 		 * Run the migrations.
 		 *
+		 * @access public
 		 * @return void
 		 */
 		public function up() {
-			
-		}
+
+			Schema::create('Gesneuvelde', function($table) {
+				$table->increments('id');
+				$table->string('Voornaam', 255);
+				$table->string('Achternaam', 45);
+				$table->string('Burgerlijke_stand', 255);
+				$table->string('Stam_nr', 30);
+				$table->integer('regiment_id');
+				$table->string('Geboren_datum', 45);
+				$table->string('Geboren_plaats', 200);
+				$table->text('Overleden_locatie');
+				$table->string('Overleden_datum', 45);
+				$table->string('Overleden_plaats', 400);
+				$table->text('Doodsoorzaak');
+				$table->integer('herdenking_id');
+				$table->string('Added', 70);
+				$table->string('Geslacht', 45);
+				$table->string('Eenheid', 40);
+				$table->string('Rang', 45);
+				$table->string('Graf_referentie', 45);
+				$table->string('Dienst', 45);
+				$table->timestamps();
+			});
+
+	}
 
 		/**
 		 * Reverse the migrations.
 		 *
+		 * @access public
 		 * @return void
 		 */
 		public function down() {
-			Schame::drop('Gesneuvelde');
+			Schema::drop('Gesneuvelde');
 		}
 
 	}
