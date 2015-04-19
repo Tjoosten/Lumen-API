bc267ccad940b3ebeb4c6926c07c29e5eb980d11
Tjoosten <Tjoosten3@gmail.com>
2015-04-20 01:31:34 +0200

    basic gulp commands

---
+++ API - source code/gulpfile.js
@@ -0,0 +1,43 @@
+// =================================================================================================
+// Includes.
+// =================================================================================================
+var gulp       = require('gulp');             // Gulp main package
+var jshint     = require('gulp-jshint');      // Gulp plug-in for checking syntax error in JS files.
+var changed    = require('gulp-changed');     // Gulp plug-in for helping gulp-imagemin.
+var imagemin   = require('gulp-imagemin');    // Gulp plug-in for minify images.
+
+// =================================================================================================
+// TASK METHODS.
+// =================================================================================================
+
+// jshint check in the JAvascript files for syntax errors.
+// [CLI Call] = gulp jshint
+gulp.task('jshint', function() {
+  gulp.src('resources/assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Minify new images
+// [CLI Call] = gulp imagemin
+gulp.task('imagemin', function() {
+  var imgSrc = 'resources/assets/img/**/*',
+      imgDst = 'public/assets/img';
+
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+
+gulp.task("one", function () {
+  gulp.src("3.txt")
+    .pipe(notify({
+      title:   "Fucker Defuq?",
+      subtitle: "Fallen Soldiers API",
+      message: "This is a message.",
+      sound: "Frog",
+      onLast: true,
+      wait: true
+    }));
+});
