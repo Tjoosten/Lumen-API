780bf0cae1ff5166a2557e5e453bb503be3fa4c2
Tjoosten <Tjoosten3@gmail.com>
2015-04-21 00:53:36 +0200

    Update callback

--- API - source code/app/Http/Controllers/apiSoldiers.php
+++ API - source code/app/Http/Controllers/apiSoldiers.php
@@ -4,10 +4,8 @@
 
   use App\Models\Soldaten;
   use App\Http\Controllers\Controller;
-
   use League\Fractal\Manager;
   use League\Fractal\Resource\Collection;
-
   use Illuminate\Http\Response;
 
   class apiSoldiers extends Controller {
@@ -32,8 +30,7 @@
     public function Soldiers($parse) {
       $soldaten           = Soldaten::with('begraafplaats', 'regiment');
       $variable['result'] = $soldaten->get();
-
-      $resource = new Collection($variable['result'], $this->transformSoldierCallback());
+      $resource           = new Collection($variable['result'], $this->transformSoldierCallback());
 
       if($parse === 'json') {
         return response($this->fractal->createData($resource)->toJson(), 200)
@@ -46,25 +43,6 @@
           'message' => 'Invalid parse option',
         ], 200)->header('Content-Type', 'application/json');
       }
-    }
-
-      /**
-       * @return callable
-       */
-      private function transformSoldierCallback() {
-        return function($data) {
-            return [
-                [
-                    'id'                => (int)    $data['id'],
-                    'Voornaam'          => (string) $data['Voornaam'],
-                    'Achternaam'        => (string) $data['Achternaam'],
-                    'Burgerlijke stand' => (string) $data['Burgerlijke_stand'],
-                    'Dienst nr'         => (string) $data['Stam_nr'],
-                    'Regiment ID'       => (int)    $data['regiment_id'],
-                    'Regiment'          => (string) $data['regiment']['Regiment'],
-                ],
-            ];
-        };
     }
 
     /**
@@ -119,6 +97,26 @@
         'error'   => false,
         'soldier' => 'Soldier deleted',
       ], 200)->header('Content-Type', 'application/json');
+    }
+
+    /**
+     * @access private
+     * @return callable
+     */
+    private function transformSoldierCallback() {
+      return function($data) {
+        return [
+          [
+            'id'                => (int)    $data['id'],
+            'Voornaam'          => (string) $data['Voornaam'],
+            'Achternaam'        => (string) $data['Achternaam'],
+            'Burgerlijke stand' => (string) $data['Burgerlijke_stand'],
+            'Dienst nr'         => (string) $data['Stam_nr'],
+            'Regiment ID'       => (int)    $data['regiment_id'],
+            'Regiment'          => (string) $data['regiment']['Regiment'],
+          ],
+        ];
+      };
     }
 
   }
