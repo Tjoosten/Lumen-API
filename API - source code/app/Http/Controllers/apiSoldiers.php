0103e152a9b223a5bc32bd9470b5af15fa450555
Tjoosten <Tjoosten3@gmail.com>
2015-04-21 14:57:46 +0200

    delete unused statement

--- API - source code/app/Http/Controllers/apiSoldiers.php
+++ API - source code/app/Http/Controllers/apiSoldiers.php
@@ -3,7 +3,6 @@
   namespace App\Http\Controllers;
 
   use App\Models\Soldaten;
-  use App\Http\Controllers\Controller;
   use League\Fractal\Manager;
   use League\Fractal\Resource\Collection;
   use Illuminate\Http\Response;
